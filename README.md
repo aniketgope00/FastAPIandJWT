589d55bbd50784a1b138a985aa3dc089b8271d71
aniketgope00 <aniketgope00@gmail.com>
2026-08-15 23:50:04 +0530

    adding db setup

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # FastAPIandJWT
+
+
+### Setting Up Repo
+```
+uv init
+uv venv myenv
+# for powershell
+myenv/Scripts/activate
+```
+
+check running containers with: ```docker ps```
+run postgres image as ```docker run --name postgres-db -e POSTGRES_USER=user -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres```
+
+
+install and build: ```uv pip install -r requirements.txt; uv sync --active```
+
+run app: ```fastapi dev main.py```
--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from fastapiandjwt!")
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.util.init_db import create_tables
 
 
-if __name__ == "__main__":
-    main()
+
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    # initialize DB at Start
+    create_tables()
+    yield #seperation point
+
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "Running"}
